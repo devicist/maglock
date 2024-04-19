2ae07de87842f2aec2831ef256f4ef8b4b4e3c69
Nicholas Stedman <nickstedman@gmail.com>
2024-04-18 21:11:23 -0400

    Update maglock.ino
    Swap enable command (ie. 1) for automatic reenable after timeout.

--- maglock/maglock.ino
+++ maglock/maglock.ino
@@ -1,9 +1,12 @@
 /*
  * USAGE
  * 
- * Send 1 to enable the maglock, send 0 to disable it.
+ * Send 0 to disable maglock.
+ * Maglock will automatically reset after timeout.
  * Send values as ASCII characters. 
  * CR and NL are ignored.
+ *
+ * Button is onboard but not used.
  * 
  * Attach to maglock via amplifier.
  * (eg. https://www.amazon.ca/gp/product/B07F7SV84V/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1)
@@ -15,10 +18,13 @@
 const int maglockControlPin = 14;
 int maglockSetter = 1;
 const int buttonPin = 12;
+long timeStamp = 0;
+const long timeout = 60000;
 
 void setup() {
   pinMode(maglockControlPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
+  digitalWrite(maglockControlPin, HIGH);
   Serial.begin(9600);
 }
 
@@ -27,9 +33,14 @@ void loop() {
     String maglockSetterString = Serial.readString();
     maglockSetterString.trim();
     maglockSetter = maglockSetterString.toInt();
-    if(maglockSetter != 0) maglockSetter = 1;
     Serial.println(maglockSetter, DEC);
+    if(maglockSetter == 0) {
+      digitalWrite(maglockControlPin, LOW);
+      timeStamp = millis();
+    }
   }
-  digitalWrite(maglockControlPin, maglockSetter);
+  if(millis() > timeStamp + timeout){
+    digitalWrite(maglockControlPin, HIGH);
+  }
   delay(5);
 }
